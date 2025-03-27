4cced87e11326e9c981609b12e9383c65dad56a7
nati-sahlu <natansahlu5@gmai.com>
2025-03-27 10:39:35 +0300

    added the links

--- js/script.js
+++ js/script.js
@@ -21,3 +21,36 @@ const highlightMenu=()=>{
     }
 }
 window.addEventListener('scroll',highlightMenu);
+const menuBtn=document.querySelector('.menu');
+const nav=document.querySelector('.nav-bar');
+const navBar=document.querySelector('.left-bar-nav');
+const escapeButton=document.querySelector('.escape-menu');
+menuBtn.addEventListener('click',()=>{
+    navBar.style.top='0px';
+    
+    nav.style.height='250px';
+    menuBtn.style.display='none';
+    escapeButton.style.display='block';
+
+
+})
+escapeButton.addEventListener('click',()=>{
+    navBar.style.top='-1000px';
+    
+    nav.style.height='80px';
+    menuBtn.style.display='block';
+    escapeButton.style.display='none';
+})
+window.addEventListener('resize', function() {
+    const mobileMenu = document.querySelector('.menu');
+    const screenWidth = window.innerWidth;
+
+    if (screenWidth > 767) {
+        mobileMenu.style.display = 'none';
+
+        
+    }
+    else{
+        mobileMenu.style.display='';
+    }
+});
